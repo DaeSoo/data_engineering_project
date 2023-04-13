8bd3d9d9b7fd34f905c7dd2ee9476f631b08fc38
woojo <woojo3398@datanuri.net>
2023-04-13 18:07:33 +0900

    초기버전 commit

---
+++ src/main/java/riot/api/data/engineer/DataEngineerApplication.java
@@ -0,0 +1,13 @@
+package riot.api.data.engineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEngineerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataEngineerApplication.class, args);
+    }
+
+}
