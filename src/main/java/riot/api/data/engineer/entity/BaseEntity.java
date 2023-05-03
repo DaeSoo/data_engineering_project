7cadd78406bbbad8de9ffbbd375fddee635a8d41
daesoo <daesoo@datanuri.net>
2023-05-03 11:43:51 +0900

    [FEAT] BaseEntity 개발 abstract class를 이용하여 Save/Update시 날짜를 자동으로 입력되게 생성

---
+++ src/main/java/riot/api/data/engineer/entity/BaseEntity.java
@@ -0,0 +1,43 @@
+package riot.api.data.engineer.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@NoArgsConstructor
+public abstract class BaseEntity {
+
+    /*
+     * 생성일
+     */
+    @Column(name = "created_date",insertable = true, updatable = false)
+    private LocalDateTime createdDate;
+
+    /*
+     * 갱신일
+     */
+    @Column(name = "last_update_date")
+    private LocalDateTime lastUpdateDate;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        lastUpdateDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        lastUpdateDate = LocalDateTime.now();
+    }
+
+}
--- src/main/java/riot/api/data/engineer/entity/UserInfo.java
+++ src/main/java/riot/api/data/engineer/entity/UserInfo.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 @Entity(name = "user_info")
 @Setter
 @Getter
-public class UserInfo {
+public class UserInfo extends BaseEntity {
     @Id
     @Column(name="summoner_id")
     @SerializedName("summonerId")
